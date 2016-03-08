b304f8bb947d3be2dc5bb7a5fb2d59e2278c2214
followNate <yuanmeng@usc.edu>
2016-03-08 01:15:09 -0800

    Update kmutex.c

--- kernel/proc/kmutex.c
+++ kernel/proc/kmutex.c
@@ -1,3 +1,21 @@
+/******************************************************************************/
+/* Important Spring 2016 CSCI 402 usage information:                          */
+/*                                                                            */
+/* This fils is part of CSCI 402 kernel programming assignments at USC.       */
+/*         53616c7465645f5f2e8d450c0c5851acd538befe33744efca0f1c4f9fb5f       */
+/*         3c8feabc561a99e53d4d21951738da923cd1c7bbd11b30a1afb11172f80b       */
+/*         984b1acfbbf8fae6ea57e0583d2610a618379293cb1de8e1e9d07e6287e8       */
+/*         de7e82f3d48866aa2009b599e92c852f7dbf7a6e573f1c7228ca34b9f368       */
+/*         faaef0c0fcf294cb                                                   */
+/* Please understand that you are NOT permitted to distribute or publically   */
+/*         display a copy of this file (or ANY PART of it) for any reason.    */
+/* If anyone (including your prospective employer) asks you to post the code, */
+/*         you must inform them that you do NOT have permissions to do so.    */
+/* You are also NOT permitted to remove or alter this comment block.          */
+/* If this comment block is removed or altered in a submitted file, 20 points */
+/*         will be deducted.                                                  */
+/******************************************************************************/
+
 #include "globals.h"
 #include "errno.h"
 
@@ -13,9 +31,11 @@
  */
 
 void
-kmutex_init(kmutex_t *mtx) {
-    sched_queue_init(&mtx->km_waitq);
-    mtx->km_holder = NULL;
+kmutex_init(kmutex_t *mtx)
+{
+        /* initialize the mutex waiting Q and let the mutex holder to be NULL */
+        sched_queue_init(&mtx->km_waitq);
+        mtx->km_holder = NULL;
 }
 
 /*
@@ -25,13 +45,22 @@ kmutex_init(kmutex_t *mtx) {
  * No thread should ever try to lock a mutex it already has locked.
  */
 void
-kmutex_lock(kmutex_t *mtx) {
-    if (mtx->km_holder != NULL){
-        KASSERT(mtx->km_holder != curthr);
+kmutex_lock(kmutex_t *mtx)
+{
+    /*
+    if(mtx->km_holder != NULL){
+        // KASSERT(mtx->km_holder != NULL,("Mutex is not available"));
         sched_sleep_on(&mtx->km_waitq);
     }
-
-    mtx->km_holder = curthr;
+    else {
+        mtx->km_holder = curthr;
+    } */
+    KASSERT(curthr && curthr != mtx->km_holder && "Current thread is the holder!!!");
+    if (mtx->km_holder) {
+		sched_sleep_on(&mtx->km_waitq);
+	} else { 
+		mtx->km_holder = curthr;
+	}
 }
 
 /*
@@ -39,19 +68,16 @@ kmutex_lock(kmutex_t *mtx) {
  * instead.
  */
 int
-kmutex_lock_cancellable(kmutex_t *mtx) {
-    if (mtx->km_holder != NULL){
-        KASSERT(mtx->km_holder != curthr);
-        int wait_result = sched_cancellable_sleep_on(&mtx->km_waitq);
-
-        if (wait_result == -EINTR){
-            return -EINTR;
-        }
+kmutex_lock_cancellable(kmutex_t *mtx)
+{
+   KASSERT(curthr && curthr != mtx->km_holder && "Current thread is the holder!!!");
+   if(mtx->km_holder != NULL){
+        /* KASSERT(mtx->km_holder != NULL,("Mutex is not available")); */
+        return sched_cancellable_sleep_on(&mtx->km_waitq);
+    } else {
+        mtx->km_holder = curthr; 
+        return 0;
     }
-
-    mtx->km_holder = curthr;
-
-    return 0;
 }
 
 /*
@@ -69,7 +95,8 @@ kmutex_lock_cancellable(kmutex_t *mtx) {
  * @param mtx the mutex to unlock
  */
 void
-kmutex_unlock(kmutex_t *mtx) {
-    kthread_t *next_holder = sched_wakeup_on(&mtx->km_waitq);
-    mtx->km_holder = next_holder;
+kmutex_unlock(kmutex_t *mtx)
+{
+     kthread_t *next_holder = sched_wakeup_on(&mtx->km_waitq);
+     mtx->km_holder = next_holder;
 }

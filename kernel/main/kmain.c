b9734e38420f0be33576571755df6fc01560f821
followNate <yuanmeng@usc.edu>
2016-03-08 01:15:39 -0800

    Update kmain.c

--- kernel/main/kmain.c
+++ kernel/main/kmain.c
@@ -1,7 +1,24 @@
+/******************************************************************************/
+/* Important Spring 2016 CSCI 402 usage information:                          */
+/*                                                                            */
+/* This fils is part of CSCI 402 kernel programming assignments at USC.       */
+/*         53616c7465645f5f2e8d450c0c5851acd538befe33744efca0f1c4f9fb5f       */
+/*         3c8feabc561a99e53d4d21951738da923cd1c7bbd11b30a1afb11172f80b       */
+/*         984b1acfbbf8fae6ea57e0583d2610a618379293cb1de8e1e9d07e6287e8       */
+/*         de7e82f3d48866aa2009b599e92c852f7dbf7a6e573f1c7228ca34b9f368       */
+/*         faaef0c0fcf294cb                                                   */
+/* Please understand that you are NOT permitted to distribute or publically   */
+/*         display a copy of this file (or ANY PART of it) for any reason.    */
+/* If anyone (including your prospective employer) asks you to post the code, */
+/*         you must inform them that you do NOT have permissions to do so.    */
+/* You are also NOT permitted to remove or alter this comment block.          */
+/* If this comment block is removed or altered in a submitted file, 20 points */
+/*         will be deducted.                                                  */
+/******************************************************************************/
+
 #include "types.h"
 #include "globals.h"
 #include "kernel.h"
-#include "errno.h"
 
 #include "util/gdb.h"
 #include "util/init.h"
@@ -42,34 +59,22 @@
 #include "fs/vfs_syscall.h"
 #include "fs/fcntl.h"
 #include "fs/stat.h"
-#include "fs/namev.c"
-
-#include "test/proctest.h"
-#include "test/ttytest.h"
-#include "test/atatest.h"
-#include "test/memdevtest.h"
-#include "test/s5fstest.h"
-#include "test/vmmtest.h"
-#include "test/kshell/customcommands.h"
 
 #include "test/kshell/kshell.h"
-#include "../test/kshell/priv.h"
-#include "../test/kshell/command.h"
+#include "errno.h"
 
-#include "test/kshell/io.h"
 GDB_DEFINE_HOOK(boot)
 GDB_DEFINE_HOOK(initialized)
 GDB_DEFINE_HOOK(shutdown)
 
+static void       hard_shutdown(void);
 static void      *bootstrap(int arg1, void *arg2);
 static void      *idleproc_run(int arg1, void *arg2);
 static kthread_t *initproc_create(void);
 static void      *initproc_run(int arg1, void *arg2);
-static void       hard_shutdown(void);
 
 static context_t bootstrap_context;
-
-int vfstest_main(int argc, char **argv);
+extern int gdb_wait;
 
 /**
  * This is the first real C function ever called. It performs a lot of
@@ -95,7 +100,7 @@ kmain()
 
         acpi_init();
         apic_init();
-	      pci_init();
+        pci_init();
         intr_init();
 
         gdt_init();
@@ -117,10 +122,41 @@ kmain()
         void *bstack = page_alloc();
         pagedir_t *bpdir = pt_get();
         KASSERT(NULL != bstack && "Ran out of memory while booting.");
+        /* This little loop gives gdb a place to synch up with weenix.  In the
+         * past the weenix command started qemu was started with -S which
+         * allowed gdb to connect and start before the boot loader ran, but
+         * since then a bug has appeared where breakpoints fail if gdb connects
+         * before the boot loader runs.  See
+         *
+         * https://bugs.launchpad.net/qemu/+bug/526653
+         *
+         * This loop (along with an additional command in init.gdb setting
+         * gdb_wait to 0) sticks weenix at a known place so gdb can join a
+         * running weenix, set gdb_wait to zero  and catch the breakpoint in
+         * bootstrap below.  See Config.mk for how to set GDBWAIT correctly.
+         *
+         * DANGER: if GDBWAIT != 0, and gdb is not running, this loop will never
+         * exit and weenix will not run.  Make SURE the GDBWAIT is set the way
+         * you expect.
+         */
+        while (gdb_wait) ;
         context_setup(&bootstrap_context, bootstrap, 0, NULL, bstack, PAGE_SIZE, bpdir);
         context_make_active(&bootstrap_context);
 
         panic("\nReturned to kmain()!!!\n");
+}
+
+/**
+ * Clears all interrupts and halts, meaning that we will never run
+ * again.
+ */
+static void
+hard_shutdown()
+{
+#ifdef __DRIVERS__
+        vt_print_shutdown();
+#endif
+        __asm__ volatile("cli; hlt");
 }
 
 /**
@@ -139,49 +175,23 @@ kmain()
 static void *
 bootstrap(int arg1, void *arg2)
 {
-    /* necessary to finalize page table information */
-    pt_template_init();
+        /* If the next line is removed/altered in your submission, 20 points will be deducted. */
+        dbgq(DBG_CORE, "SIGNATURE: 53616c7465645f5f37b32598398c8c19c81dafd1a37f23165b3a631091a982882cd05017e4ede71d134c5c196281c438\n");
+        /* necessary to finalize page table information */
+        pt_template_init();
 
-    char *name = "idle process";
-
-    proc_t *idle_proc = proc_create(name);
-
-    if (idle_proc == NULL){
-        panic("idle proc is NULL :( \n");
-    }
-
-    KASSERT(idle_proc->p_pid == 0);
-
-    kthread_t *idle_thread = kthread_create(idle_proc, idleproc_run, NULL, NULL);
-
-    if (idle_thread == NULL){
-        panic("idle thread is NULL :( \n");
-    }
-
-    curproc = idle_proc;
-    curthr = idle_thread;
-
-    context_make_active(&idle_thread->kt_ctx);   
-
-    panic("weenix returned to bootstrap()!!! BAD!!!\n");
-    return NULL;
-}
-
-static void destroy_kshell_commands(){
-    list_t *commands = &kshell_commands_list;
-    list_link_t *link = commands->l_next;
-
-    while (link != commands){
-        kshell_command_t *cmd = list_item(link, kshell_command_t, kc_commands_link);
-
+        curproc = proc_create("idle process");
+        KASSERT(curproc != NULL);
+        KASSERT(curproc->p_pid == (pid_t) PID_IDLE);
         
+        curthr = kthread_create(curproc, idleproc_run, arg1, arg2);
+        KASSERT(curthr != NULL);
         
-        link = link->l_next;
+        /* this function would never return. */
+        context_make_active(&(curthr->kt_ctx));
 
-         if (cmd != NULL){
-            kshell_command_destroy(cmd);
-        }
-    }
+        panic("weenix returned to bootstrap()!!! BAD!!!\n");
+        return NULL;
 }
 
 /**
@@ -199,110 +209,65 @@ static void destroy_kshell_commands(){
 static void *
 idleproc_run(int arg1, void *arg2)
 {
-    int status;
-    pid_t child;
+        int status;
+        pid_t child;
 
-    /* create init proc */
-    kthread_t *initthr = initproc_create();
+        /* create init proc */
+        kthread_t *initthr = initproc_create();
+        init_call_all();
+        GDB_CALL_HOOK(initialized);
 
-    if (initthr == NULL){
-        panic("couldn't create init proc\n");
-    }
-
-    init_call_all();
-    GDB_CALL_HOOK(initialized);
-
-    /* Create other kernel threads (in order) */
+        /* Create other kernel threads (in order) */
 
 #ifdef __VFS__
-    /* Once you have VFS remember to set the current working directory
-     * of the idle and init processes */
-    curproc->p_cwd = vfs_root_vn;
-    vref(vfs_root_vn); 
+        /* Once you have VFS remember to set the current working directory
+         * of the idle and init processes */
+        NOT_YET_IMPLEMENTED("VFS: idleproc_run");
 
-    initthr->kt_proc->p_cwd = vfs_root_vn;
-    vref(vfs_root_vn);
-
-    /* Here you need to make the null, zero, and tty devices using mknod 
-     You can't do this until you have VFS, check the include/drivers/dev.h
-     file for macros with the device ID's you will need to pass to mknod */
-    int mkdir_res = do_mkdir("/dev");
-
-    if (mkdir_res == 0){
-        if (do_mknod("/dev/tty0", S_IFCHR, MKDEVID(2, 0)) < 0){
-            panic("unable to create tty0\n");
-        }
-
-        if (do_mknod("/dev/tty1", S_IFCHR, MKDEVID(2, 1)) < 0){
-            panic("unable to create tty1\n");
-        }
-
-        if (do_mknod("/dev/tty2", S_IFCHR, MKDEVID(2, 2)) < 0){
-            panic("unable to create tty2\n");
-        }
-
-        /*if (do_mknod("/dev/null", S_IFBLK, MEM_NULL_DEVID) < 0){*/
-        if (do_mknod("/dev/null", S_IFCHR, MEM_NULL_DEVID) < 0){
-            panic("unable to create /dev/null");
-        } 
-
-        if (do_mknod("/dev/zero", S_IFCHR, MEM_ZERO_DEVID) < 0){
-            panic("unable to create /dev/zero");
-        }
-    } else {
-        KASSERT(mkdir_res == -EEXIST && "wrong type of error when making /dev");
-    }
-
-    int mktmp_res = do_mkdir("/tmp");
-
-    KASSERT((mkdir_res == 0 || mkdir_res == -EEXIST) && "wront type of error \
-            making /tmp");
-
-    /*kmutex_init(&lookup_mutex);*/
-
+        /* Here you need to make the null, zero, and tty devices using mknod */
+        /* You can't do this until you have VFS, check the include/drivers/dev.h
+         * file for macros with the device ID's you will need to pass to mknod */
+        NOT_YET_IMPLEMENTED("VFS: idleproc_run");
 #endif
 
-    /* Finally, enable interrupts (we want to make sure interrupts
-     * are enabled AFTER all drivers are initialized) */
-    intr_enable();
+        /* Finally, enable interrupts (we want to make sure interrupts
+         * are enabled AFTER all drivers are initialized) */
+        intr_enable();
 
-    /* Run initproc */
-    sched_make_runnable(initthr);
-
-    /* Now wait for it */
-    child = do_waitpid(-1, 0, &status);
-    KASSERT(PID_INIT == child);
-
-    destroy_kshell_commands();
+        /* Run initproc */
+        sched_make_runnable(initthr);
+        /* Now wait for it */
+        child = do_waitpid(-1, 0, &status);
+        KASSERT(PID_INIT == child);
 
 #ifdef __MTP__
-    kthread_reapd_shutdown();
+        kthread_reapd_shutdown();
 #endif
 
 
 #ifdef __SHADOWD__
-    /* wait for shadowd to shutdown */
-    shadowd_shutdown();
+        /* wait for shadowd to shutdown */
+        shadowd_shutdown();
 #endif
 
 #ifdef __VFS__
-    /* Shutdown the vfs: */
-    dbg_print("weenix: vfs shutdown...\n");
-    vput(curproc->p_cwd);
-    if (vfs_shutdown())
-        panic("vfs shutdown FAILED!!\n");
+        /* Shutdown the vfs: */
+        dbg_print("weenix: vfs shutdown...\n");
+        vput(curproc->p_cwd);
+        if (vfs_shutdown())
+                panic("vfs shutdown FAILED!!\n");
 
 #endif
 
-    /* Shutdown the pframe system */
+        /* Shutdown the pframe system */
 #ifdef __S5FS__
-    pframe_shutdown();
+        pframe_shutdown();
 #endif
 
-    dbg_print("\nweenix: halted cleanly!\n");
-    GDB_CALL_HOOK(shutdown);
-    hard_shutdown();
-    return NULL;
+        dbg_print("\nweenix: halted cleanly!\n");
+        GDB_CALL_HOOK(shutdown);
+        hard_shutdown();
+        return NULL;
 }
 
 /**
@@ -318,24 +283,24 @@ idleproc_run(int arg1, void *arg2)
 static kthread_t *
 initproc_create(void)
 {
-    proc_t *initproc = proc_create("init proc");
-
-    if (initproc == NULL){
-        return NULL;
-    }
-
-    KASSERT(initproc->p_pid == (pid_t) 1 && "initproc pid isn't 1");
-
-    kthread_t *init_thread = kthread_create(initproc, initproc_run, NULL, NULL);
-
-    return init_thread;
+        proc_t * initProc = proc_create("init process");
+        if (initProc == NULL) {
+                return NULL;
+        }
+        
+        /* make sure the PID is 1 and create a thread for it. */
+        KASSERT(initProc->p_pid == (pid_t) PID_INIT);
+        kthread_t *thr = kthread_create(initProc, initproc_run, NULL, NULL);
+        KASSERT(thr != NULL);
+        
+        return thr;
 }
 
 /**
  * The init thread's function changes depending on how far along your Weenix is
  * developed. Before VM/FI, you'll probably just want to have this run whatever
  * tests you've written (possibly in a new process). After VM/FI, you'll just
- * exec "/bin/init".
+ * exec "/sbin/init".
  *
  * Both arguments are unused.
  *
@@ -345,41 +310,11 @@ initproc_create(void)
 static void *
 initproc_run(int arg1, void *arg2)
 {
-    static char bullshit[1000];
-    /*run_vmm_tests();*/
+        char *empty_args[2] = {"init", NULL};
+        char *empty_envp[1] = {NULL};
+        
+        kernel_execve("/sbin/init", empty_args, empty_envp);
+        panic("oh shit");
 
-    /*kshell_add_command("exec", kshell_exec, "executes a given command");*/
-
-    char *empty_args[2] = {"init", NULL};
-    char *empty_envp[1] = {NULL};
-    /*kernel_execve("/usr/bin/hello", empty_args, empty_envp);*/
-    kernel_execve("/sbin/init", empty_args, empty_envp);
-    panic("oh shit");
-    
-    /*run_proc_tests();*/
-    /*run_tty_tests();*/
-    /*run_memdev_tests();*/
-    /*[>run_ata_tests();<]*/
-
-/*    int i;*/
-    /*for (i = 0; i < 4; i++){*/
-        /*run_s5fs_tests();*/
-    /*}*/
-    
-    /*vfstest_main(1, NULL);   */
-    
-    return NULL;
-}
-
-/**
- * Clears all interrupts and halts, meaning that we will never run
- * again.
- */
-static void
-hard_shutdown()
-{
-#ifdef __DRIVERS__
-        vt_print_shutdown();
-#endif
-        __asm__ volatile("cli; hlt");
+        return NULL;
 }
